75ad1dc93719367fce6ccaef68b34acae565ea4a
csBhoomika <84519557+csBhoomika@users.noreply.github.com>
2021-06-15 12:49:43 +0530

    solved

--- Program-2.c
+++ Program-2.c
@@ -5,12 +5,13 @@
 int main()
 {
 	int i,n;
-	printf("Enter the number of elements ");
+	printf("Enter the number of elements \n");
 	scanf("%d",&n);
 	for(i=1;i<=n;i++)
   {
     	printf("%d\t",i);
   }
+  printf("\n");
 return 0;
 	//Fill the missing code here
  }
